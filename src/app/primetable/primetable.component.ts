8bd09d84a0dc1a4114d494d0bb6ff26c4242d208
Ege Darçın <edarcin@gmail.com>
2018-07-04 16:31:42 +0300

    Sort fixed, selected row fixed, lower fillers deleted.

--- src/app/primetable/primetable.component.ts
+++ src/app/primetable/primetable.component.ts
@@ -11,10 +11,9 @@ import {Message} from 'primeng/api';
 export class PrimetableComponent implements OnInit {
 
   heroes: Hero[];
-  displayDialog: boolean;
+  displayDialogAdd: boolean;
+  displayDialogDisp: boolean;
   selectedHero: Hero;
-  newHero: boolean;
-  msgs: Message[] = [];
   cols: any[];
 
   constructor(private heroService: HeroService) { }
@@ -46,43 +45,19 @@ export class PrimetableComponent implements OnInit {
   this.heroService.deleteHero(hero).subscribe();
   }
   showDialogToAdd() {
-    this.newHero = true;
-    this.displayDialog = true;
+    this.displayDialogAdd = true;
   }
   /*
- onRowSelect(event) {
-   this.newHero = false;
-   this.Hero = this.cloneHero(event.data);
-   this.displayDialog = true;
- }
+  showDialogToDisp() {
+    this.displayDialogDisp = true;
+  }
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe();
+  }
 
- cloneHero(c: Hero): Hero {
-   let car = {};
-   for (let prop in c) {
-     car[prop] = c[prop];
-   }
-   return car;
- }
+  getHero(): void {
 
- customSort(event: SortEvent) {
-   event.data.sort((data1, data2) => {
-     let value1 = data1[event.field];
-     let value2 = data2[event.field];
-     let result = null;
-
-     if (value1 == null && value2 != null)
-       result = -1;
-     else if (value1 != null && value2 == null)
-       result = 1;
-     else if (value1 == null && value2 == null)
-       result = 0;
-     else if (typeof value1 === 'string' && typeof value2 === 'string')
-       result = value1.localeCompare(value2);
-     else
-       result = (value1 < value2) ? -1 : (value1 > value2) ? 1 : 0;
-
-     return (event.order * result);
-   });
- }
- */
+  }
+  */
 }
